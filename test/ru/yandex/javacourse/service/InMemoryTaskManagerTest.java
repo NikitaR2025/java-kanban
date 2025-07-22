775cc8d3bd25834d481296a1207748fca81a42ed
Nikita <nikita.rotkin@yandex.ru>
2025-07-22 09:44:04 +0300

    добавил @DisplayName в InMemoryTaskManagerTest

--- test/ru/yandex/javacourse/service/InMemoryTaskManagerTest.java
+++ test/ru/yandex/javacourse/service/InMemoryTaskManagerTest.java
@@ -1,6 +1,7 @@
 package ru.yandex.javacourse.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.yandex.javacourse.model.Epic;
 import ru.yandex.javacourse.model.Subtask;
@@ -15,9 +16,11 @@ class InMemoryTaskManagerTest {
     Task task;
     Epic epic;
     Subtask subtask;
+    int listTasksSize;
 
     @BeforeEach
-    void createManager() {
+    @DisplayName("Инициализирует manager, создает task, epic, subtask и добавляет их в manager")
+    void setUp() {
         manager = Managers.getDefault();
         task = new Task("title", "description");
         manager.addTask(task);
@@ -28,7 +31,23 @@ class InMemoryTaskManagerTest {
     }
 
     @Test
-    void addTask(){
+    @DisplayName("Проверяет что при добавлении Task, Subtask, Epic увеличивается длина соответствующего списка задач")
+    void addTask() {
+        listTasksSize = manager.getTasks().size();
+        manager.addTask(task);
+        assertEquals(++listTasksSize, manager.getTasks().size(), "Не увеличилась длина списка Task");
+        listTasksSize = manager.getSubtasks().size();
+        manager.addTask(subtask);
+        assertEquals(++listTasksSize, manager.getSubtasks().size(), "Не увеличилась длина списка Subtask");
+        listTasksSize = manager.getEpics().size();
+        manager.addTask(epic);
+        assertEquals(++listTasksSize, manager.getEpics().size(), "Не увеличилась длина списка Epics");
+    }
+
+
+    @Test
+    @DisplayName("Проверяет, что методы getTasks, getSubtasks, getEpics возвращают списки задач, и списки не пустые")
+    void getTasks() {
         assertNotNull(manager.getTasks(), "Список задач не возвращается");
         assertEquals(1, manager.getTasks().size(), "Список задач не должен быть пуст");
         assertNotNull(manager.getSubtasks(), "Список Подзадач не возвращается");
@@ -38,22 +57,25 @@ class InMemoryTaskManagerTest {
     }
 
     @Test
-    void updateTask(){
-        int listTasksSize = manager.getTasks().size();
+    @DisplayName("Проверяет, что при вызове метода updateTask количество задач в списке не изменяется")
+    void updateTask() {
+        listTasksSize = manager.getTasks().size();
         task.setDescription("Изменен description");
         manager.updateTask(task);
         assertEquals(listTasksSize, manager.getTasks().size(), "Изменилось количество задач");
     }
 
     @Test
-    void removeTask(){
-        int listTasksSize = manager.getTasks().size();
+    @DisplayName("Проверяет, что при вызове метода removeTask количество задач уменьшилось на 1")
+    void removeTask() {
+        listTasksSize = manager.getTasks().size();
         manager.removeTask(1);
         assertEquals(--listTasksSize, manager.getTasks().size(), "Не уменьшилось количество задач");
     }
 
     @Test
-    void removeTasks(){
+    @DisplayName("Проверяет, что при вызове метода removeTasks все списки задач становятся пустыми")
+    void removeTasks() {
         manager.removeTasks();
         assertTrue(manager.getTasks().isEmpty(), "Список Задач не очищен");
         manager.removeSubtasks();
@@ -62,29 +84,26 @@ class InMemoryTaskManagerTest {
         assertTrue(manager.getEpics().isEmpty(), "Список Эпиков не очищен");
     }
 
-
     @Test
+    @DisplayName("Проверяет, что поля задач, полученных методами getTask, getSubtask, getEpic соответствуют полям сохраненных задач")
     void shouldReturnEqualFieldsInTask() {
         assertEquals(task.toString(), manager.getTask(1).toString(), "Поля сохраненной  и полученной Задачи не совпадают");
         assertEquals(subtask.toString(), manager.getSubtask(3).toString(), "Поля сохраненной  и полученной Подзадачи не совпадают");
         assertEquals(epic.toString(), manager.getEpic(2).toString(), "Поля сохраненного  и полученного Эпика не совпадают");
-
     }
 
     @Test
-    void getSubtasksOfEpic(){
+    @DisplayName("Проверяет, что метод getSubtasksOfEpic возвращает список подзадач эпика, и список не пустой")
+    void getSubtasksOfEpic() {
         assertNotNull(manager.getSubtasksOfEpic(2), "Список подзадач эпика не возвращается");
         assertNotEquals(0, manager.getSubtasksOfEpic(2).size(), "Список подзадач не должен быть пуст");
     }
 
     @Test
-    void shouldBeEqualInHistory(){
+    @DisplayName("Проверяет, что последняя задача в списке истории соответствует последней запрошенной задаче")
+    void shouldBeEqualInHistory() {
         manager.getTask(1);
         manager.getSubtask(3);
         assertEquals(subtask, manager.getHistory().getLast(), "Неправильная последняя задача в истории");
     }
-
-
-
-
 }
