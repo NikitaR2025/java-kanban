f0a1ecb2809e0251300590debabcec0b766b7a17
Nikita <nikita.rotkin@yandex.ru>
2025-08-29 07:22:55 +0300

    удалить тест переполнения  истории 10 элементов

--- test/ru/yandex/javacourse/service/InMemoryHistoryManagerTest.java
+++ test/ru/yandex/javacourse/service/InMemoryHistoryManagerTest.java
@@ -20,24 +20,6 @@ public class InMemoryHistoryManagerTest {
     }
 
     @Test
-    @DisplayName("Проверяет, что при заполнении списка истории 11 элементов первый элемент удаляется")
-    void add_add11Elements_shouldRemoveFirstElement() {
-        //given
-        Task task;
-        for (int i = 1; i <= 10; i++) {
-            task = new Task("task", "description");
-            task.setId(i);
-            historyManager.add(task);
-        }
-        //when
-        task = new Task("task", "description");
-        task.setId(11);
-        historyManager.add(task);
-        //then
-        assertEquals(2, historyManager.getHistory().getFirst().getId(), "Первый элемент не удалился");
-    }
-
-    @Test
     @DisplayName("Проверяет, что метод getHistory возвращает ArrayList")
     void getHistory_shouldReturnNotNullList() {
         //when
